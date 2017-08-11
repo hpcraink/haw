189f9e654d9e5eac05512615d82db1b614a0be4d
Alexandru Saramet <alexandru@saramet.eu>
2017-08-11 17:50:36 +0200

    first backend tests /user requests and mongodb

--- backend/spec/REST.spec.js
+++ backend/spec/REST.spec.js
@@ -1,12 +1,15 @@
 'use strict';
 
 const request = require('supertest');
-const app = require('../bin/app');
+const bcrypt = require('bcryptjs');
 
-//const app = "https://localhost:3000";
+const MongoClient = require('mongodb').MongoClient;
 
-describe("REST responces for '/' path", () => {
-  it("get('/') returns a predifined message", (done) => {
+const app = require('../app/app');
+const mongodb_uri = "mongodb://localhost:27017/bwUniCluster"
+
+describe("GET '/'", () => {
+  it("responds with a greeting", (done) => {
     request(app).get('/')
       .expect(200)
       .expect("Background server is up!")
@@ -14,6 +17,42 @@ describe("REST responces for '/' path", () => {
   })
 });
 
-describe("REST for '/user'", () => {
-  it()
-})
+describe("REST /user'", () => {
+  // create a test user Object
+  const User = require('../models/user');
+  const user = new User({
+    firstName: "Test REST",
+    lastName: "Test User REST",
+    password: bcrypt.hashSync('testPass', 10),
+    email: 'test@test.rest',
+  });
+
+  // delete test objects from database
+  afterAll((done) => {
+    // Set up the connection to the local db
+    MongoClient.connect(mongodb_uri, (err, db) => {
+      // From 'users' collection remove 'user' doc
+      db.collection('users').deleteMany({lastName: "Test User REST"}, (err, res) => {
+        if (err) throw err;
+        db.close();
+        done();
+      });
+    });
+  });
+
+  // Tests
+  it("'/user' doesn't create an unvalidated user", (done) => {
+    request(app).post('/user')
+      //.send(unvalid)
+      .send({firstName: "Unvalid", lastName: "Test User REST"})
+      .expect(500) // will display the error message on testing, should be ignored as test is success
+      .end(error => error ? done.fail() : done());
+  });
+
+  it("'/user' creates a new user", (done) => {
+    request(app).post('/user')
+      .send(user)
+      .expect(201)
+      .end(error => error ? done.fail(error) : done());
+  });
+});
--- backend/spec/mongodb.spec.js
+++ backend/spec/mongodb.spec.js
@@ -1,9 +1,61 @@
-describe("MongoDB tests", function() {
-  it("is there a server running", function(done) {
-    const MongoClient = require('mongodb').MongoClient;
-    MongoClient.connect('mongodb://127.0.0.1:27017', function(err, db) {
-      expect(err).toBe(null);
+'use strict';
+
+const MongoClient = require('mongodb').MongoClient;
+const Server = require('mongodb').Server;
+const bcrypt = require('bcryptjs');
+
+const db_uri = "mongodb://localhost:27017/bwUniCluster"
+
+describe("MongoDB tests if", function() {
+
+  // a test user Object
+  const user = {
+    firstName: "Test",
+    lastName: "Test User",
+    email: 'test@test.info',
+  };
+
+  beforeAll((done) => {
+    // insert test user to the database
+    MongoClient.connect(db_uri, function(err, db) {
+      db.collection('users').insertOne(user, (err, res) => {
+        if (err) throw err;
+        db.close();
+        done();
+      })
+    });
+  });
+
+  afterAll((done) => {
+    // remove test user from the database
+    MongoClient.connect(db_uri, function(err, db) {
+      db.collection('users').deleteOne(user, (err, res) => {
+        if (err) throw err;
+        db.close();
+        done();
+      })
+    });
+  });
+
+  it("mongo server is running and a 'bwUniCluster database exists'", function(done) {
+    MongoClient.connect(db_uri, function(err, db) {
+      expect(err).toBeNull();
+      expect(db).not.toBeNull();
+      expect(db).not.toBeUndefined();
+      db.close();
       done();
+    });
+  });
+
+  it("user is succesifully inserted", (done) => {
+    MongoClient.connect(db_uri, function(err, db) {
+      db.collection('users').findOne({lastName: 'Test User'}, (err, result) => {
+        expect(err).toBeNull();
+        expect(result.firstName).toBe("Test");
+        expect(result.email).toBe('test@test.info');
+        db.close();
+        done();
+      })
     });
   });
 });
